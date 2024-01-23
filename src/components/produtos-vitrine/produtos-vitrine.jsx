3f858db63705a9c6912ccf42c566210b815a2682
abvictor <victor.amaralb@hotmail.com>
2024-01-23 18:33:51 -0300

    style: fix product card design

--- src/components/produtos-vitrine/produtos-vitrine.jsx
+++ src/components/produtos-vitrine/produtos-vitrine.jsx
@@ -1,19 +1,24 @@
+import foto from "../../assets/hamburguer.png";
+import bag from "../../assets/bag-black.png";
+
+
 import "./produto-vitrine.css"
 
 function ProdutoVitrine(){
     return (
         <div className="produto-box text-center">
-            <img src={""} alt="Imagem do hambúrger" />
+            <img src={foto} alt="Foto" />
             
             <div>
-                <h2 className="prod-vitrine-descricao">Spicy Burguer</h2> 
+                <h2>Spicy Burguer</h2>
+                <p className="prod-vitrine-descricao">Hamburguer de 250g, queijo, tomate, alface e cebola</p>
                 <p className="prod-vitrine-preco">R$ 18,90</p>
             </div>
 
             <div>
                 <button className="btn btn-cart">
-                    <img src={""} className="icon"/>
-                    Adiconar
+                    <img src={bag} className="icon" alt='bag icon' />
+                    Adicionar
                 </button>
             </div>
         </div>
