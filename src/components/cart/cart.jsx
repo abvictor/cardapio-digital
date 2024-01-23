0a6e7cc235d1a431e16cb921005ac6f6091d8274
abvictor <victor.amaralb@hotmail.com>
2024-01-23 20:20:44 -0300

    chore: add react-dock lib

---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,17 @@
+import { Dock } from 'react-dock'
+
+function Cart() {
+    return (
+        <Dock position='right'
+             isVisible={true}
+             fluid={false}
+             size={420}
+        >
+
+
+            
+        </Dock>
+        )
+}
+
+export default Cart;
