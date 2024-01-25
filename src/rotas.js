6076f0caccbcc4f2230425189af0e185f105b710
abvictor <victor.amaralb@hotmail.com>
2024-01-24 21:47:48 -0300

    feat: add routes

--- src/rotas.js
+++ src/rotas.js
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Checkout from './pages/Checkout/Checkout';
-import Historico from './pages/Historico/Historico';
-import Home from './components/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Checkout from "./pages/Checkout/Checkout.jsx";
+import Historico from "./pages/Historico/Historico.jsx";
 
-function Rotas() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/checkout' element={<Checkout/>}/>
-                <Route path='/historico' element={<Historico/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+function Rotas(){
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/historico" element={<Historico />} />
+        </Routes>
+    </BrowserRouter>
 }
 
 export default Rotas;
