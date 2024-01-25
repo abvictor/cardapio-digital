2343a60c525f4ae21539256e4d743c5f175fce34
abvictor <victor.amaralb@hotmail.com>
2024-01-24 21:52:40 -0300

    feat: create navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,30 @@
 import logo from '../../assets/logo.png' 
 import bag from "../../assets/bag.png";
+import Cart from '../cart/cart';
+
 import "./navbar.css"
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
     
+    function openSideBar() {
+        const event = new CustomEvent('openSideBar')
+        window.dispatchEvent(event)
+    }
+
+    const navigate = useNavigate
     
     return (
         <nav>
             <img src={logo} alt="Logotipo" className="logotipo"/>
             <div className="menu">
-                <a href="#">Histórico</a>
-                <button className="btn btn-red">
+                <a onClick={navigate('/checkout')}>Histórico</a>
+                <button className="btn btn-red" onClick={openSideBar}>
                     <img src={bag} className="icon" alt='bag icon' />
                     Sacola
                 </button>
             </div>
+            <Cart/>
         </nav>
     )
 }
